d6f88f7a5c83ed8e41c5fc9b8a7047d3a423d26e
mymircle0118 <noahsflood908@gmail.com>
2024-03-25 21:01:25 +0900

    modify clie

--- Cli/src/index.ts
+++ Cli/src/index.ts
@@ -25,18 +25,16 @@ import {AccountLayout,MintLayout,TOKEN_PROGRAM_ID,Token,ASSOCIATED_TOKEN_PROGRAM
 import { program } from 'commander';
 import { programs } from '@metaplex/js';
 import log from 'loglevel';
-// import axios from "axios"
-// import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
+import axios from "axios"
+import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
 
 program.version('0.0.1');
 log.setLevel('info');
 
-const PERSONAL_DATA_SIZE = 32 + 32 + 8 + 4 + 88;
-
 // const programId = new PublicKey('AirdfxxqajyegRGW1RpY5JfPyYiZ2Z9WYAZxmhKzxoKo')
-const programId = new PublicKey('8PUdnFDjVnuFu5fcUnQnKxUtZTGpyua3nD8LAh5yc5pw')
-const mintkey = new PublicKey('jmsApix74A2RUJyw5XRCCL61MiCRP538yypQmemnCZd')
-const pool_address = new PublicKey('HH2m1spAiWsQYgmRy8EqMihGffHr7sipsGVzFZrXfEWp')
+const programId = new PublicKey('Hq6uN7rEysDbBQDSte5JXLwuhKX8NJ3MsDAwvhdJhgkv')
+const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
+const pool_address = new PublicKey('9C6LsMVXabiPNVG4gJQVYV1Rg76Wd6jDZpekVvC9ti4J')
 const idl=JSON.parse(fs.readFileSync('src/solana_anchor.json','utf8'))
 const { metadata: { Metadata } } = programs
 
@@ -61,116 +59,116 @@ function loadWalletKey(keypair : any): Keypair {
   return loaded;
 }
 
-function loadWallefFromSecretKey(key : any) {
-
-  let byte_array = bs58.decode(key)
-
-  const loaded = Keypair.fromSecretKey(
-    new Uint8Array(byte_array),
-  );
-
-  log.info(`wallet public key: ${loaded.publicKey}`);
-
-  return loaded;
-  
+const getTokenWallet = async (
+  wallet: anchor.web3.PublicKey,
+  mint: anchor.web3.PublicKey
+    ) => {
+  return (
+    await anchor.web3.PublicKey.findProgramAddress(
+      [wallet.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mint.toBuffer()],
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+  )[0];
 }
 
-async function getSetupAccount(owner : PublicKey, pool: PublicKey, token: PublicKey) {
-  // console.log(owner, pool, token);
-  return await PublicKey.findProgramAddress([owner.toBuffer(),pool.toBuffer(), token.toBuffer()], programId)
+const getMetadata = async (
+  mint: anchor.web3.PublicKey
+    ): Promise<anchor.web3.PublicKey> => {
+  return (
+    await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mint.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    )
+  )[0];
+};
+
+const createAssociatedTokenAccountInstruction = (
+  associatedTokenAddress: PublicKey,
+  payer: PublicKey,
+  walletAddress: PublicKey,
+  splTokenMintAddress: PublicKey
+    ) => {
+  const keys = [
+    { pubkey: payer, isSigner: true, isWritable: true },
+    { pubkey: associatedTokenAddress, isSigner: false, isWritable: true },
+    { pubkey: walletAddress, isSigner: false, isWritable: false },
+    { pubkey: splTokenMintAddress, isSigner: false, isWritable: false },
+    {
+      pubkey: SystemProgram.programId,
+      isSigner: false,
+      isWritable: false,
+    },
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+    {
+      pubkey: SYSVAR_RENT_PUBKEY,
+      isSigner: false,
+      isWritable: false,
+    },
+  ];
+  return new TransactionInstruction({
+    keys,
+    programId: ASSOCIATED_TOKEN_PROGRAM_ID,
+    data: Buffer.from([]),
+  });
 }
 
-async function sendTransaction(conn : any, wallet : any, transaction : Transaction, signers : Keypair[]) {
-  try{
-    transaction.feePayer = wallet.publicKey
-    transaction.recentBlockhash = (await conn.getRecentBlockhash('max')).blockhash;
-    // await transaction.setSigners(wallet.publicKey,...signers.map(s => s.publicKey));
-    if(signers.length != 0)
-      await transaction.partialSign(...signers)
-    const signedTransaction = await wallet.signTransaction(transaction);
-    let hash = await conn.sendRawTransaction(await signedTransaction.serialize());
-    await conn.confirmTransaction(hash);
-  } catch(err) {
-    console.log(err)
-    return false;
-  }
-  return true;
+async function getDecimalsOfToken(conn : Connection, mint : PublicKey){
+  let resp = await conn.getAccountInfo(mint)
+  let accountData = MintLayout.decode(Buffer.from(resp!.data))
+  return accountData.decimals
 }
-
-// programCommand('initstake')
-// .requiredOption(
-//   '-i, --info <path>',
-//   'secret info location')
-// .action(async (directory, cmd) => {
-//   try{
-//     const {env, info} = cmd.opts()
-//     const conn = new Connection(clusterApiUrl(env))
-//     const infoJson = JSON.parse(fs.readFileSync(info).toString())
-//     const keystr = infoJson.privatekey
-//     const owner = loadWallefFromSecretKey(keystr)
-//     const wallet = new anchor.Wallet(owner)
-//     const provider = new anchor.Provider(conn,wallet,confirmOption)
-//     const program = new anchor.Program(idl,programId,provider)
-//     const rand = Keypair.generate().publicKey;
-
-//     let [stakeState, bump] = await getStakeStateAccount(wallet.publicKey, pool_address, mintkey);
-
-//     console.log(mintkey.toBase58())
-    
-//     let transaction = new Transaction()
-
-//     transaction.add(
-//       await program.instruction.initStakeState(new anchor.BN(bump),
-//       {
-//         accounts: {
-//           owner : wallet.publicKey,
-//           pool : pool_address,
-//           soulsMint: mintkey,
-//           stakeState: stakeState,
-//           systemProgram : anchor.web3.SystemProgram.programId
-//         }
-//       })
-//     )
-
-//     // sendTransaction(conn, wallet, transaction, [owner]);
-
-//     const hash = await sendAndConfirmTransaction(conn, transaction, [owner], confirmOption)
-
-//     console.log("POOL : "+ pool_address.toBase58())
-//     console.log("personal address", stakeState.toBase58())
-
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })
 
 programCommand('init_pool')
   .requiredOption(
-    '-i, --info <path>',
+    '-k, --keypair <path>',
     'Solana wallet location'
+  )
+  .requiredOption(
+    '-i, --info <path>',
+    'Schedule info location'
   )
   .action(async (directory,cmd)=>{
     try{
-    const {env, info} = cmd.opts()
+    const {env,keypair,info} = cmd.opts()
     const conn = new Connection(clusterApiUrl(env))
-    // const owner = loadWalletKey(keypair)
-    const infoJson = JSON.parse(fs.readFileSync(info).toString())
-    const keystr = infoJson.privatekey
-    const owner = loadWallefFromSecretKey(keystr)
+    const owner = loadWalletKey(keypair)
     const wallet = new anchor.Wallet(owner)
     const provider = new anchor.Provider(conn,wallet,confirmOption)
     const program = new anchor.Program(idl,programId,provider)
     const rand = Keypair.generate().publicKey;
     const [pool, bump] = await PublicKey.findProgramAddress([rand.toBuffer()],programId)
     let transaction = new Transaction()
-
+    const infoJson = JSON.parse(fs.readFileSync(info).toString())
+    const tokenMint = new PublicKey(infoJson.token)
+    const tokenAccount = await getTokenWallet(pool, tokenMint)
+    transaction.add(createAssociatedTokenAccountInstruction(tokenAccount, owner.publicKey, pool, tokenMint))
+    const decimals = Math.pow(10,await getDecimalsOfToken(conn,tokenMint))
+    console.log("token decimal", decimals);
     transaction.add(program.instruction.initPool(
       new anchor.BN(bump),
+      new anchor.BN(infoJson.staking_period),
+      new anchor.BN(infoJson.withdraw_period),
+      new anchor.BN(infoJson.start_time),
+      new anchor.BN(infoJson.soul_amount),
+      new anchor.BN(infoJson.win_percent),
+      new anchor.BN(infoJson.fail_percent),
+      new anchor.BN(infoJson.burn_percent),
+      new anchor.BN(infoJson.token_unit),
+      infoJson.stake_collection,
+      infoJson.token_halos,
+      infoJson.token_horns,
+      new anchor.BN(infoJson.period),
       {
         accounts:{
           owner : owner.publicKey,
           pool : pool,
           rand : rand,
+          soulsMint : tokenMint,
+          tokenProgram : TOKEN_PROGRAM_ID,
           systemProgram : SystemProgram.programId
         }
       }
@@ -196,199 +194,93 @@ programCommand('get_pool')
     const provider = new anchor.Provider(conn,wallet,confirmOption)
     const program = new anchor.Program(idl,programId,provider)
     const poolData = await program.account.pool.fetch(poolAddress)
-    console.log("Pool Data", poolData);
+    // const resp = await conn.getTokenAccountBalance(poolData.souls_mint, "max")
+    // const amount = resp.value.uiAmountString
+    // const decimals = Math.pow(10,resp.value.decimals)
+    console.log("test");
+    console.log("        Pool Data");
     console.log("Owner : " + poolData.owner.toBase58())
-    console.log("Rand : " + poolData.rand.toBase58())
-    console.log("key : " + poolData.skey.toBase58())
-    console.log("bump : " + poolData.bump)
+    console.log("Token : " + poolData.soulsMint.toBase58())
+    console.log("total souls : " + poolData.totalSouls.toNumber())
+    console.log("staking period : " + poolData.stakingPeriod.toNumber() + " day")
+    console.log("withdraw period : " + poolData.withdrawPeriod.toNumber() + " day")
+    console.log("halos count", poolData.halosCount.toNumber());
+    console.log("horns_count", poolData.hornsCount.toNumber());
+    console.log("soul amount", poolData.soulAmount.toNumber());
+    console.log("start time", poolData.startTime.toNumber());
+    console.log("token unit", poolData.tokenUnit.toNumber());
+    console.log("stake collection", poolData.stakeCollection);
+    console.log("token halos", poolData.tokenHalos);
+    console.log("token horns", poolData.tokenHorns);
+    console.log("win percent", poolData.winPercent.toNumber());
+    console.log("fail percent", poolData.failPercent.toNumber());
+    console.log("burn percent", poolData.burnPercent.toNumber());
+    console.log("period", poolData.period.toNumber());
+    console.log("when                   amount");
+    // (poolData.schedule as any[]).map((item) => {
+    //   console.log((new Date(item!.airdropTime*1000)).toLocaleString(),"      ",item!.airdropAmount/decimals)
+    // })
+    console.log("")
   })
 
-  programCommand('initstake')
-  .requiredOption(
-    '-i, --info <path>',
-    'secret info location')
-  .action(async (directory, cmd) => {
-    try{
-      const {env, info} = cmd.opts()
-      const conn = new Connection(clusterApiUrl(env))
-      const infoJson = JSON.parse(fs.readFileSync(info).toString())
-      const keystr = infoJson.privatekey
-      const owner = loadWallefFromSecretKey(keystr)
-      const wallet = new anchor.Wallet(owner)
-      const provider = new anchor.Provider(conn,wallet,confirmOption)
-      const program = new anchor.Program(idl,programId,provider)
-      // const rand = Keypair.generate().publicKey;
-  
-      let [stakeState, bump] = await getSetupAccount(wallet.publicKey, pool_address, mintkey);
-  
-      console.log(mintkey.toBase58())
-      
-      let transaction = new Transaction()
-  
-      transaction.add(
-        await program.instruction.initStakeState(new anchor.BN(bump),
-        {
-          accounts: {
-            owner : wallet.publicKey,
-            pool : pool_address,
-            soulsMint: mintkey,
-            stakeState: stakeState,
-            systemProgram : anchor.web3.SystemProgram.programId
-          }
-        })
-      )
-  
-      // sendTransaction(conn, wallet, transaction, [owner]);
-  
-      const hash = await sendAndConfirmTransaction(conn, transaction, [owner], confirmOption)
-  
-      console.log("POOL : "+ pool_address.toBase58())
-      console.log("personal address", stakeState.toBase58())
-  
-    } catch (err) {
-      console.log(err);
-    }
-  })
+  programCommand('get_nft')
+  .option(
+    '-k, --keypair <path>',
+    'keypair path'
+  ).action(async (directory,cmd)=>{
+    const {env, keypair} = cmd.opts()
+    const conn = new Connection(clusterApiUrl(env))
+    const owner = loadWalletKey(keypair)
+    // const provider = new anchor.Provider(conn,owner,confirmOption)
 
-  programCommand('setup_env')
-  .requiredOption(
-    '-i, --info <path>',
-    'secret info location')
-  .action(async (directory, cmd) => {
-    try{
-      const {env, info} = cmd.opts()
-      const conn = new Connection(clusterApiUrl(env))
-      const infoJson = JSON.parse(fs.readFileSync(info).toString())
-      const keystr = infoJson.privatekey
-      console.log(keystr.length)
-      const owner = loadWallefFromSecretKey(keystr)
-      const wallet = new anchor.Wallet(owner)
-      const provider = new anchor.Provider(conn, wallet, confirmOption)
-      const program = new anchor.Program(idl,programId,provider)
-      const rand = Keypair.generate().publicKey;
-      // const account_buf = Buffer.from(anchor.utils.bytes.utf8.encode("individual"))
-      let [personal_account, bump] = await getSetupAccount(wallet.publicKey, pool_address, mintkey)
+    const allTokens: any = []
+    const tokenAccounts = await conn.getParsedTokenAccountsByOwner(owner.publicKey, {
+      programId: TOKEN_PROGRAM_ID
+    });
 
-      // console.log("personal account", personal_account.toBase58())
-      // console.log("mintkey", mintkey.toBase58())
-      // console.log("owner", wallet.publicKey.toBase58())
-
-      let transaction = new Transaction()
-
-      if((await conn.getAccountInfo(personal_account)) != null) {
-        console.log("test")
-        return;
-      }
-
-      transaction.add(program.instruction.setup(
-        new anchor.BN(bump),
-        keystr,
-        {
-          accounts:{
-            owner : wallet.publicKey,
-            pool : pool_address,
-            mintKey : mintkey,
-            personData : personal_account,
-            clock : SYSVAR_CLOCK_PUBKEY,
-            systemProgram : SystemProgram.programId
-          }
-        }
-      ))
-
-      // sendTransaction(conn, wallet, transaction, [owner]);
-
-      const hash = await sendAndConfirmTransaction(conn, transaction, [owner], confirmOption)
-
-      console.log("POOL : "+ pool_address.toBase58())
-      console.log("personal address", personal_account.toBase58())
-
-    } catch (err) {
-      console.log(err);
-    }
-  })
-
-  
-  programCommand('init_pool')
-    .requiredOption(
-      '-i, --info <path>',
-      'Solana wallet location'
-    )
-    .action(async (directory,cmd)=>{
+    for (let index = 0; index < tokenAccounts.value.length; index++) {
       try{
-      const {env, info} = cmd.opts()
-      const conn = new Connection(clusterApiUrl(env))
-      // const owner = loadWalletKey(keypair)
-      const infoJson = JSON.parse(fs.readFileSync(info).toString())
-      const keystr = infoJson.privatekey
-      const owner = loadWallefFromSecretKey(keystr)
-      const wallet = new anchor.Wallet(owner)
-      const provider = new anchor.Provider(conn,wallet,confirmOption)
-      const program = new anchor.Program(idl,programId,provider)
-      const rand = Keypair.generate().publicKey;
-      const [pool, bump] = await PublicKey.findProgramAddress([rand.toBuffer()],programId)
-      let transaction = new Transaction()
-  
-      transaction.add(program.instruction.initPool(
-        new anchor.BN(bump),
-        {
-          accounts:{
-            owner : owner.publicKey,
-            pool : pool,
-            rand : rand,
-            systemProgram : SystemProgram.programId
+        const tokenAccount = tokenAccounts.value[index];
+        const tokenAmount = tokenAccount.account.data.parsed.info.tokenAmount;
+        if (tokenAmount.amount == "1" && tokenAmount.decimals == "0") {
+          let nftMint = new PublicKey(tokenAccount.account.data.parsed.info.mint)
+          let [pda] = await anchor.web3.PublicKey.findProgramAddress([
+            Buffer.from("metadata"),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            nftMint.toBuffer(),
+          ], TOKEN_METADATA_PROGRAM_ID);
+          const accountInfo: any = await conn.getParsedAccountInfo(pda);
+          let metadata : any = new Metadata(owner.publicKey.toString(), accountInfo.value);
+          console.log("test meatadata", metadata.data.data)
+          const { data }: any = await axios.get(metadata.data.data.uri)
+          console.log("nft get data", data)
+          if (true) {
+            const entireData = { ...data, id: Number(data.name.replace( /^\D+/g, '').split(' - ')[0]) }
+            console.log("nft data", entireData);
+            console.log("hvh type",entireData.attributes[0].value)
+            allTokens.push({account_address : tokenAccount.pubkey, mint_address : nftMint, ...entireData, mname : metadata.data.data.name })
           }
         }
-      ))
-      const hash = await sendAndConfirmTransaction(conn, transaction, [owner], confirmOption)
-      console.log("POOL : "+pool.toBase58())
-      console.log("Transaction ID : " + hash)
-      }catch(err){
-        console.log(err)
+        allTokens.sort(function (a: any, b: any) {
+          if (a.name < b.name) { return -1; }
+          if (a.name > b.name) { return 1; }
+          return 0;
+        })
+      } catch(err) {
+        continue;
       }
-    })
-  
-  programCommand('get_info')
-  .requiredOption(
-    '-i, --info <path>',
-    'Solana wallet location'
-  )
-  .action(async (directory,cmd)=>{
-    try{
-      const {env, info} = cmd.opts()
-      const conn = new Connection(clusterApiUrl(env))
-      // const owner = loadWalletKey(keypair)
-      const infoJson = JSON.parse(fs.readFileSync(info).toString())
-      const keystr = infoJson.privatekey
-      const owner = loadWallefFromSecretKey(keystr)
-      const wallet = new anchor.Wallet(owner)
-      const provider = new anchor.Provider(conn,wallet,confirmOption)
-      const program = new anchor.Program(idl,programId,provider)
-
-      let resp = await conn.getProgramAccounts(programId,{
-        dataSlice: {length: 0, offset: 0},
-        filters: [{dataSize: 8 + PERSONAL_DATA_SIZE}, {memcmp:{offset:40,bytes:pool_address.toBase58()}}]
-      })
-
-      console.log("count of private keys", resp.length)
-      let personal_data
-
-      for(let account of resp){
-        personal_data = await program.account.personalData.fetch(account.pubkey)
-        console.log("private key", personal_data.ownerKey)
-      }
-    } catch(error) {
-      console.log(error)
     }
+
+    console.log("")
   })
 
-  
 function programCommand(name: string) {
   return program
     .command(name)
     .option(
       '-e, --env <string>',
       'Solana cluster env name',
-      'devnet',
+      'mainnet-beta',
     )
     .option('-l, --log-level <string>', 'log level', setLogLevel);
 }
